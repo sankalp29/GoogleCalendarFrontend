e0bca9a657cedfe7907eb69e13f82ab51745466a
Sankalp Bhagwat <sankalp.29bhagwat@gmail.com>
2025-10-29 18:22:20 +0530

    Merged interactions into created UI

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import './App.css';
-import React, { useState, useContext, useEffect } from 'react';
-import { getMonth } from './util'
-import CalendarHeader from './components/CalendarHeader';
-import Sidebar from './components/Sidebar';
-import Month from './components/Month';
-import GlobalContext from './context/GlobalContext';
-import EventModal from './components/EventModal';
-
+import React, { useState, useContext, useEffect } from "react";
+import "./App.css";
+import { getMonth } from "./util";
+import CalendarHeader from "./components/CalendarHeader";
+import Sidebar from "./components/Sidebar";
+import Month from "./components/Month";
+import GlobalContext from "./context/GlobalContext";
+import EventModal from "./components/EventModal";
 function App() {
-  const [currentMonth, setCurrentMonth] = useState(getMonth())
+  const [currenMonth, setCurrentMonth] = useState(getMonth());
   const { monthIndex, showEventModal } = useContext(GlobalContext);
+
   useEffect(() => {
     setCurrentMonth(getMonth(monthIndex));
   }, [monthIndex]);
@@ -17,12 +17,13 @@ function App() {
   return (
     <React.Fragment>
       {showEventModal && <EventModal />}
-      <div className='h-screen flex flex-col'>
+
+      <div className="h-screen flex flex-col">
         <CalendarHeader />
-          <div className='flex flex-1'>
-            <Sidebar />
-            <Month month={currentMonth}/>
-          </div>
+        <div className="flex flex-1">
+          <Sidebar />
+          <Month month={currenMonth} />
+        </div>
       </div>
     </React.Fragment>
   );
--- src/context/ContextWrapper.js
+++ src/context/ContextWrapper.js
@@ -1,56 +1,116 @@
-import React, { useState, useEffect, useReducer } from 'react'
-import GlobalContext from './GlobalContext'
-import dayjs from 'dayjs'
+import React, {
+    useState,
+    useEffect,
+    useReducer,
+    useMemo,
+} from "react";
 
-function savedEventsReducer(state, {type, payload}) {
+import GlobalContext from "./GlobalContext";
+import dayjs from "dayjs";
+  
+function savedEventsReducer(state, { type, payload }) {
     switch (type) {
-        case "push":
-            return [...state, payload]
-        case "update":
-            return state.map(evt => evt.id === payload.id ? payload : evt)
-        case "delete":
-            return state.filter(evt => evt.id !== payload.id)
-        default:
-            throw new Error("Invalid action type")
+      case "push":
+        return [...state, payload];
+      case "update":
+        return state.map((evt) =>
+          evt.id === payload.id ? payload : evt
+        );
+      case "delete":
+        return state.filter((evt) => evt.id !== payload.id);
+      default:
+        throw new Error();
     }
 }
 
 function initEvents() {
     const storageEvents = localStorage.getItem("savedEvents");
-    return storageEvents ? JSON.parse(storageEvents) : [];
+    const parsedEvents = storageEvents ? JSON.parse(storageEvents) : [];
+    return parsedEvents;
 }
-
+  
 export default function ContextWrapper(props) {
     const [monthIndex, setMonthIndex] = useState(dayjs().month());
     const [smallCalendarMonth, setSmallCalendarMonth] = useState(null);
     const [daySelected, setDaySelected] = useState(dayjs());
     const [showEventModal, setShowEventModal] = useState(false);
-    const [savedEvents, dispatchCalEvent] = useReducer(savedEventsReducer, [], initEvents)
-
+    const [selectedEvent, setSelectedEvent] = useState(null);
+    const [labels, setLabels] = useState([]);
+    const [savedEvents, dispatchCalEvent] = useReducer(
+      savedEventsReducer,
+      [],
+      initEvents
+    );
+  
+    const filteredEvents = useMemo(() => {
+      return savedEvents.filter((evt) =>
+        labels
+          .filter((lbl) => lbl.checked)
+          .map((lbl) => lbl.label)
+          .includes(evt.label)
+      );
+    }, [savedEvents, labels]);
+  
     useEffect(() => {
-        localStorage.setItem("savedEvents", JSON.stringify(savedEvents));
-    }, [savedEvents])
-
+      localStorage.setItem("savedEvents", JSON.stringify(savedEvents));
+    }, [savedEvents]);
+  
     useEffect(() => {
-        if (smallCalendarMonth !== null) {
-            setMonthIndex(smallCalendarMonth);
-        }
-    }, [smallCalendarMonth])
+      setLabels((prevLabels) => {
+        return [...new Set(savedEvents.map((evt) => evt.label))].map(
+          (label) => {
+            const currentLabel = prevLabels.find(
+              (lbl) => lbl.label === label
+            );
+            return {
+              label,
+              checked: currentLabel ? currentLabel.checked : true,
+            };
+          }
+        );
+      });
+    }, [savedEvents]);
+  
+    useEffect(() => {
+      if (smallCalendarMonth !== null) {
+        setMonthIndex(smallCalendarMonth);
+      }
+    }, [smallCalendarMonth]);
+  
+    useEffect(() => {
+      if (!showEventModal) {
+        setSelectedEvent(null);
+      }
+    }, [showEventModal]);
+  
+    function updateLabel(label) {
+      setLabels(
+        labels.map((lbl) => (lbl.label === label.label ? label : lbl))
+      );
+    }
+  
     return (
-        <GlobalContext.Provider 
-            value={{ 
-                monthIndex, 
-                setMonthIndex, 
-                smallCalendarMonth, 
-                setSmallCalendarMonth, 
-                daySelected, 
-                setDaySelected,
-                showEventModal,
-                setShowEventModal,
-                savedEvents,
-                dispatchCalEvent
-            }}>
-            {props.children}
-        </GlobalContext.Provider>
-    )
+      <GlobalContext.Provider
+        value={{
+          monthIndex,
+          setMonthIndex,
+          smallCalendarMonth,
+          setSmallCalendarMonth,
+          daySelected,
+          setDaySelected,
+          showEventModal,
+          setShowEventModal,
+          dispatchCalEvent,
+          selectedEvent,
+          setSelectedEvent,
+          savedEvents,
+          setLabels,
+          labels,
+          updateLabel,
+          filteredEvents,
+        }}
+      >
+        {props.children}
+      </GlobalContext.Provider>
+    );
 }
